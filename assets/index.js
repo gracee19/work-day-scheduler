74659ff7acca97a9ae7ebe2e1f063416e2dee447
Grace Palenzuela <gracee_palenzuela@yahoo.com>
2021-01-18 17:42:55 +1030

    updated js,html and css

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,49 @@
-var cDate = $("#currentDay")
+$(document).ready(function () {
+    var events = [];
+
+    //Set current date in the Jumbotron class
+    var cDate = $('#currentDay');
+
+    //Saving event in the local storage 
+    var events = JSON.parse(localStorage.getItem("events")) || [];
+    var checkStorage = localStorage.getItem("events");
+
+    $(".saveBtn").on('click',function() {
+        $('.time-block').each(function() {
+        var term = $('.time-block').data('id');
+        // alert(term);
+        console.log("hi");
+        });
+        
+    });
+    // //Creating a row inside a loop
+    // for (var i = 0; i < 24; i++) {
 
 
-cDate.text(moment().format("dddd, MMMM Do YYYY"));
+
+    //     // Check and apply local storage
+    //     if (checkStorage == null) {
+
+    //         events.push({ eventId: i });
+    //         localStorage.setItem('events', JSON.stringify(events));
+
+    //     };
+        
+
+    //     // Create Grid - Event
+    //     var eventDescCol = $('<div class="description col-8">');
+    //     eventDescCol.attr('data-time', clockIn);
+
+    //     // Set Timeframe Class
+    //     if (moment(clockIn).isBefore(now)) {
+    //         eventDescCol.addClass('past');
+    //     } else if (moment(clockIn).isSame(now)) {
+    //         eventDescCol.addClass('present');
+    //     } else if (moment(clockIn).isAfter(now)) {
+    //         eventDescCol.addClass('future');
+    //     };
+    // };
+
+    //Display the current date in the scree.
+  cDate.text(moment().format("dddd, MMMM Do YYYY"));
+});  
